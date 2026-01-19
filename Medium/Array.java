418da379a4de71f22ce6a108524608c14ba9f314
Kamal Singh Solanki <solankikamalsingh89@gmail.com>
2026-01-19 23:12:37 +0530

    Day - 16

--- Medium/Array.java
+++ Medium/Array.java
@@ -1,5 +1,6 @@
 package Medium;
 public class Array {
+    //Sort array of 0,1,2
     public void sortColors(int[] nums) { // Dutch National Flag algorithm
         int low=0, mid=0, high=nums.length-1;
         while(mid<=high){
@@ -13,5 +14,19 @@ public class Array {
             }
             else mid++;
         }
+    }
+
+    //Majority Element(>n/2)
+    public int majorityElement(int[] nums) {//Optimal approach is better as we will refresh only if c==0 ho jyega, as mentioned >n/2 element will be there
+        int e=0,c=0;
+        for(int i=0;i<nums.length;i++){
+            if(c==0){
+                e=nums[i];
+                c++;
+            }
+            else if(nums[i]==e) c++;
+            else c--;
+        }
+        return e;
     }
 }
