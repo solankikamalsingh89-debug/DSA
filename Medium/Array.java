71462f11d33f8dc2acd6e9bb19818119b54dc176
Kamal Singh Solanki <solankikamalsingh89@gmail.com>
2026-01-21 23:45:51 +0530

    Day - 18

--- Medium/Array.java
+++ Medium/Array.java
@@ -52,4 +52,23 @@ public class Array {
         }
         return p;
     }
+
+    //Rearrange array element by sign
+    public int[] rearrangeBySign(int[] A) {
+        int n = A.length;
+        int[] ans = new int[n];
+        int posIndex = 0, negIndex = 1;
+        for (int i = 0; i < n; i++) {
+            if (A[i] < 0) {
+                ans[negIndex] = A[i];
+                negIndex += 2;
+            } else {
+                ans[posIndex] = A[i];
+                posIndex += 2;
+            }
+        }
+
+        return ans;
+    }
+
 }
