6a7270c1c074d1d40cc9b9c8a791c988c24fc466
Kamal Singh Solanki <solankikamalsingh89@gmail.com>
2026-01-22 23:52:00 +0530

    Day - 19

--- Medium/Array.java
+++ Medium/Array.java
@@ -71,4 +71,41 @@ public class Array {
         return ans;
     }
 
+
+    //Next Permutation
+    public void nextPermutation(int[] nums) {
+        int i=nums.length-2;
+        if(i==-1){
+            return;
+        }
+        int pr=nums[i+1];
+        for(;i>=0;i--){
+            if(pr>nums[i]){
+                break;
+            }
+            pr=nums[i];
+        }
+        if(i==-1){
+            for(int j=0;j<(nums.length)/2;j++){
+                int t=nums[j];
+                nums[j]=nums[nums.length-j-1];
+                nums[nums.length-j-1]=t;
+            }
+        }
+        else{
+            for(int j=nums.length-1;j>i;j--){
+                if(nums[j]>nums[i]){
+                    int t=nums[j];
+                    nums[j]=nums[i];
+                    nums[i]=t;
+                    break;
+                }
+            }
+            for(int j=1;j<=(nums.length-i-1)/2;j++){
+                int t=nums[i+j];
+                nums[i+j]=nums[nums.length-j];
+                nums[nums.length-j]=t;
+            }
+        }
+    }
 }
