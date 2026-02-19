a35e8777814e27d2e55ac615d21b96ab0789cb8d
Kamal Singh Solanki <solankikamalsingh89@gmail.com>
2026-02-19 23:40:13 +0530

    Day - 28

--- Hard/Array.java
+++ Hard/Array.java
@@ -154,5 +154,14 @@ public class Array {
         return ans;
     }
 
-    
+    //
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1,j=n-1,k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]) nums1[k--]=nums1[i--];
+            else nums1[k--]=nums2[j--];
+        }
+        //while(i>=0) nums1[k--]=nums1[i--]; iske toh isme honge hi so no need
+        while(j>=0) nums1[k--]=nums2[j--];
+    }
 }
